4632c9ddb29dfc2472e11eeb46e8345b70efa6f8
EHPLaptop\Eliot Holton-Park <holtonpe@sheridancollege.ca>
2025-10-23 01:01:52 -0400

    Revert "Fixing the main menu"
    This reverts commit 54796869826bc53bd6bd4ccf9b5891ccfdad5769.

--- BigBallBarrysGameJam/Assets/Scritps/MainMenu.cs
+++ BigBallBarrysGameJam/Assets/Scritps/MainMenu.cs
@@ -23,7 +23,7 @@ public class MainMenu : MonoBehaviour
 
     public void playGame()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene("MainGameScene");
     }
 
     public void hoverOver()
